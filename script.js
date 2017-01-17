1331b1f510856c0fdf110cf00800f7591f8a3b40
Emmanuel Hernandez <emmanuel.h.student@lyonscommunityschool.org>
2017-01-17 15:25:19 +0000

    I changed stuff

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
     //Write all your jQuery code here
-        $("button").click(function() {
-      $("#like").html("You like this project!");
+        $("header").click(function() {
+      $("#color").css("background-color","red");
     });
     
-    $("body").click(function(){
-        $("change").html("background-color","red")
-    });
-    
-    
-});
